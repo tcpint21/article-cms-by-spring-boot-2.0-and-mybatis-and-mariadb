6fc8a251f26ff16f08ebc0c32022b2672631dde9
tcpint21 <tcpint21@naver.com>
2019-09-10 15:06:23 +0900

    게시물리스트를 자바버전과 el, jstl  버전 2가지로 생성

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -4,27 +4,26 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sbs.starter.dto.Article;
 import com.sbs.starter.service.ArticleService;
 
-import jline.internal.Log;
 import lombok.extern.slf4j.Slf4j;
-
 
 @Controller
 @Slf4j
 public class ArticleController {
 	@Autowired
 	ArticleService articleService;
-	
+
 	@RequestMapping("/article/list")
-	public String showMain() {
+	public String showMain(Model aModel) {
 		List<Article> list = articleService.getList();
-		
-		Log.info("list : " + list);
-		
+
+		aModel.addAttribute("list", list);
+
 		return "article/list";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/article/list.jsp
+++ src/main/webapp/WEB-INF/jsp/article/list.jsp
@@ -1,12 +1,22 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page import="java.util.*"%>
+<%@ page import="com.sbs.starter.dto.Article"%>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>안녕</title>
+<title>커뮤니티 사이트 - 게시물 리스트</title>
 </head>
 <body>
-	게시물 리스트 입니다.
+	<h1>게시물 리스트 입니다.</h1>
+
+	<h2>v2</h2>
+	<c:forEach items="${list}" var="article">
+		<section>번호 : ${article.id}, 제목 : ${article.title}</section>
+		<hr>
+	</c:forEach>
 </body>
 </html>
