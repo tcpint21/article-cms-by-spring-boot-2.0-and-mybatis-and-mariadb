d5ec8d48a226842317ca104f30bb5c5d5b824cc5
tcpint21 <tcpint21@naver.com>
2019-09-17 10:02:27 +0900

    doAdd 서비스가 INSERT에 의해 자동으로 만들어진 id 값을 리턴하도록 처리

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -1,11 +1,14 @@
 package com.sbs.starter.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sbs.starter.dto.Article;
 import com.sbs.starter.service.ArticleService;
@@ -31,4 +34,22 @@ public class ArticleController {
 	public String showAdd() {
 		return "article/add";
 	}	
+	
+	@RequestMapping("/article/doAdd")
+	@ResponseBody
+	public String doAdd(@RequestParam Map<String, Object> param) {
+		long newId = articleService.add(param);
+
+		String msg = newId + "번 게시물이 추가되었습니다.";
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("alert('" + msg + "');");
+		sb.append("location.replace('./detail?id=" + newId + "');");
+
+		sb.insert(0, "<script>");
+		sb.append("</script>");
+
+		return sb.toString();
+	}		
 }
--- src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
+++ src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
@@ -1,15 +1,14 @@
 package com.sbs.starter.service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sbs.starter.dao.ArticleDao;
 import com.sbs.starter.dto.Article;
-
-import groovy.transform.AutoImplement;
+import com.sbs.starter.util.CUtil;
 
 @Service
 public class ArticleServiceImpl implements ArticleService {
@@ -17,5 +16,11 @@ public class ArticleServiceImpl implements ArticleService {
 	ArticleDao articleDao;
 	public List<Article> getList(){
 		return articleDao.getList();
+	}
+	
+	@Override
+	public long add(Map<String, Object> param) {
+		articleDao.add(param);
+		return CUtil.getAsLong(param.get("id"));
 	}
 }
